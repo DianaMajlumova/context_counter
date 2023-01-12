afb25a9b6055365bb68e0532f1834e31bd9f450c
FezileChelebiyeva <fezile.celebiyevaa@gmail.com>
2023-01-12 10:35:28 +0400

    context counter

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <MainContextProvider>
+      <App />
+    </MainContextProvider>
+  </React.StrictMode>
+);
